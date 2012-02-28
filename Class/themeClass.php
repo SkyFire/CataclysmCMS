54cde0c8be6c83a304e8a7ca62f92718178885d4
Kennen <kennen@alcenet.info>
2012-02-28 11:45:22 +0100

    - Theme system added

--- Class/themeClass.php
+++ Class/themeClass.php
@@ -8,7 +8,26 @@
 */
 
 class themeClass
-{
+    {
 
-}
+        
+        function loader_template($theme) {
+            switch ($theme) {
+                case "header":
+                    include("Theme/$theme.php");
+                    break;
+                
+                case "footer":
+                    include("Theme/$theme.php");
+                    break;
+                
+                case "block":
+                    include("Theme/$theme.php");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
+ini_set('display_errors', 1); 
+ini_set('log_errors', 1); 
+
+error_reporting(E_ALL);
+
+
+/*
+ * INCLUDES
+ */
+include("Config/config.php");       // configuracion general
+include("Class/themeClass.php");    // objeto del template
+
+/*
+ * OBJETOS
+ */
+$theme = new themeClass();
+
+
+# Cargamos el template
+$theme->loader_template("header");
 
 ?>
