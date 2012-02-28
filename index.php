dad2a909243414fc0d340f3fdf22008895b5c5d8
Kennen <kennen@alcenet.info>
2012-02-28 11:47:17 +0100

    - Added security object

--- index.php
+++ index.php
@@ -8,8 +8,9 @@ error_reporting(E_ALL);
 /*
  * INCLUDES
  */
-include("Config/config.php");       // configuracion general
-include("Class/themeClass.php");    // objeto del template
+include("Config/config.php");           // configuracion general
+include("Class/themeClass.php");        // objeto del template
+include("Class/securityClass.php");     // objeto de seguridad
 
 /*
  * OBJETOS
@@ -17,7 +18,14 @@ include("Class/themeClass.php");    // objeto del template
 $theme = new themeClass();
 
 
-# Cargamos el template
+# Cargamos el template de la cabecera
 $theme->loader_template("header");
 
+
+
+
+
+
+# Cargamos el template del pie
+$theme->loader_template("footer");
 ?>
